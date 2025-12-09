854917f999149c50157e5830872b2608a1b05f4f
teslasdev <teslasdeveloper@gmail.com>
2025-12-09 20:33:20 +0100

    push files

--- public/data/zcash/zcash/zcashShieldedMetrics/blockFees/listBlockTXs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-block="${1}"   #1 represent 1st argument
-
-./toCurl.sh getblock $block | jq .tx | jq 'reverse'.[]
--- public/data/zcash/zcash/zcashShieldedMetrics/jsonToCSV.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-json="${1}"
-
-jq -r '(map(keys) | add | unique) as $cols | map(. as $row | $cols | map($row[.])) as $rows | $cols, $rows[] | @csv' $json > $json.csv
